db6e0999888341744a6e2bb0fc4eec387aeed01f
Rajesh Jain <rjain15@gmail.com>
2019-06-19 11:15:00 -0400

    Changes - Upstream

--- git-upstream.sh
+++ git-upstream.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-git remote set-url origin apollocoder/strapi-docker.git
-git pull origin master
+git remote add upstream https://github.com/apollocoder/strapi-docker.git
+git remote -v
+git fetch upstream
 git status
-git remote set-url origin rjain15/strapi-docker.git
 git add .
 git commit -am "Changes from Upstream"
 git push origin master
